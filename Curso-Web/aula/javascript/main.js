558b48cbecb9ab9212299721352f124328cc47d6
Raquel Roldo <raquelroldo@gmail.com>
2018-02-05 10:34:01 +0000

    teste zoom

---
+++ Curso-Web/aula/javascript/main.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+	$('#collection').click(function(){
+		$('.collection-item').toggle('slow')
+	});
+	$('#site').click(function(){
+		$('.site-item').toggle('slow')
+	});
+	$('#shop').click(function(){
+		$('.shop-item').toggle('slow')
+	});
+	$('#social').click(function(){
+		$('.social-item').toggle('slow')
+	});
+});
