63723816d99b21f5a46f27b82d8cee022eb6526c
Raquel Roldo <raquelroldo@gmail.com>
2018-02-06 00:12:56 +0000

    implemented slick carrousel, override css classes

--- Curso-Web/aula/js/main.js
+++ Curso-Web/aula/js/main.js
@@ -1,36 +1,26 @@
 $(document).ready(function(){
 
-/*
-    $('.slider').slick({
-    	dots: false,
-    	infinite: true,
-    	speed:300,
-    	slidesToShow:5,
-    	slidesToScroll:5,
-	 });
-	*/ 
 	$( window ).resize(function() {
 
+		var elementPersonalIsActive = ($('.personal').attr("data-isActive") === 'true');
+		var elementBasicIsActive = ($('.basic').attr("data-isActive") === 'true');
+
 		if (window.matchMedia("(min-width: 700px)").matches) {
-			if($('.personal').attr("data-isactive") === "true"){
-				$('.personal').addClass("show-content");
+			if(elementPersonalIsActive){
+				showContent('.personal');
 			}
-			if($('.basic').attr("data-isactive") === "true"){
-				$('.basic').addClass("show-content");
+			if(elementBasicIsActive){
+				showContent('.basic');
 			}
 		} else if (window.matchMedia("(min-width: 900px)").matches){
-			if($('.personal').attr("data-isactive") === "true"){
-				console.log("< 900 >= 700");
-				$('.personal').addClass("show-content");
+			if(elementPersonalIsActive){
+				showContent('.personal');
 			}
 		} else {
-			if($('.personal').attr("data-isactive") === "true"){
-				console.log(">= 900");
-				if($('.personal').attr("data-isactive") === "true"){
-					$('.personal').addClass("show-content");
-				}
-				if($('.basic').attr("data-isactive") === "true"){
-					$('.basic').addClass("show-content");
+			if(elementPersonalIsActive){
+				showContent('.personal');
+				if(elementBasicIsActive){
+					showContent('.basic');
 				}
 			}	
 		}
@@ -55,27 +45,26 @@ $(document).ready(function(){
 				showContent('.basic');
 			}
 		}
-	
-		function showContent(className){
-			$(className).addClass("show-content");
-			$(className).attr("data-isactive", true);
-		}
-	
-		function hideContent(className){
-			$(className).removeClass("show-content");
-			$(className).attr("data-isactive", false);
-		}
-
 	});
 
+	function showContent(className){
+		$(className).addClass("show-content");
+		$(className).attr("data-isactive", true);
+	}
+
+	function hideContent(className){
+		$(className).removeClass("show-content");
+		$(className).attr("data-isactive", false);
+	}
 
 
-	$('.slider-coco').slick({
-		
+	$('.slider-images').slick({
+
+		arrows: true,
 		infinite: true,
 		speed: 100,
 		slidesToShow: 5,
-		slidesToScroll: 4,
+		slidesToScroll: 5,
 		responsive: [
 			{
 				breakpoint: 1050,
@@ -84,45 +73,29 @@ $(document).ready(function(){
 					slidesToScroll: 4
 				}
 			},
-				{
-					breakpoint: 950,
-					settings: {
-						slidesToShow: 3,
-						slidesToScroll: 3
-					}
-				},
-				{
-					breakpoint: 700,
-					settings: {
-						slidesToShow: 3,
-						slidesToScroll: 3
-					}
-				},
-				{
-					breakpoint: 600,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 2
-					}
-				},
-				{
-					breakpoint: 500,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 2
-					}
-				},
-				{
-					breakpoint: 400,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
+			{
+				breakpoint: 950,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
 				}
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
 		]
 
 	});
-		  
-
-
+	
 });
